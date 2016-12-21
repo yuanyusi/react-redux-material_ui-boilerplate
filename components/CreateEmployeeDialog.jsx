f86f4d4617a407f14b24db3377b20ccc4c855a97
Yuan Yusiyana <yuan.yusiyana@mitrais.com>
2016-12-21 18:57:29 +0700

    add others tab

--- components/CreateEmployeeDialog.jsx
+++ components/CreateEmployeeDialog.jsx
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import SelectFieldSimple from '../components/SelectFieldSimple';
-//import UploadPhoto from '../components/UploadPhoto';
-//import UploadFilePreview from '../components/UploadFilePreview';
+import SelectFieldDivision from '../components/SelectFieldDivision';
+import ImageUpload from '../components/ImageUpload';
 
 import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
@@ -13,6 +13,7 @@ import {RadioButton, RadioButtonGroup} from 'material-ui/RadioButton';
 import {
 	    FABButton,
 		Icon,
+		IconButton,
 		Grid,
 		Cell,
 		Textfield,
@@ -69,47 +70,100 @@ export default class CreateEmployeeDialog extends React.Component {
           onRequestClose={this.handleClose}
           autoScrollBodyContent={true}
         >
-<Grid style={{margin: '0px'}}>
-								<Cell col={4} style={{width: 'calc(50%)'}}>
-								
-								
-											<Textfield
-												onChange={() => {}}
-												label="Text..."
-												floatingLabel
-												style={{width: '300px'}}
-											/>
-											
-											<Textfield
-												onChange={() => {}}
-												pattern="-?[0-9]*(\.[0-9]+)?"
-												error="Input is not a number!"
-												label="Number..."
-												floatingLabel
-											/>
-											
-											<Textfield
-												onChange={() => {}}
-												label="Text lines..."
-												rows={3}
-												style={{width: '300px'}}
-												floatingLabel
-											/>
+		<div>
+				<Grid style={{margin: '0px', padding: '0px 0px 0px 12px'}}>
+					<Cell col={6} style={{minWidth: '270px'}}>							
+						
+						<TextField
+						  hintText="First Name"
+						  floatingLabelText="First Name"
+						  floatingLabelFixed={true}
+						/>
+						
+						<TextField
+						  hintText="Last Name"
+						  floatingLabelText="Last Name"
+						  floatingLabelFixed={true}
+						/>
+						
+						<TextField
+						  hintText="Gender"
+						  floatingLabelText="Gender"
+						  floatingLabelFixed={true}
+						/>
+						<div >
+						<IconButton name = 'date_range' style = {{float: 'left', top: '30px', left:'220px', color:'rgba(0, 0, 0, 0.298039)' }}/>
+						<DatePicker 
+							floatingLabelText="Date Of Birth"
+							floatingLabelFixed={true}
+							hintText="Date of Birth" 
+							container="inline" 
+							autoOk={true}
+							textFieldStyle = {{display: 'inherit', position: 'relative'}}
+						/>		
+						</div>
+						<TextField
+						  hintText="Nationality"
+						  floatingLabelText="Nationality"
+						  floatingLabelFixed={true}
+						/>
+						
+						<TextField
+						  hintText="Marital Status"
+						  floatingLabelText="Marital Status"
+						  floatingLabelFixed={true}
+						/>
+						
+						<TextField
+						  hintText="Phone"
+						  floatingLabelText="Phone"
+						  floatingLabelFixed={true}
+						/>
+					</Cell>
+				<Cell col={5} style={{minWidth: '270px'}}>
+									<TextField
+									  hintText="Sub Division"
+									  floatingLabelText="Sub Division"
+									  floatingLabelFixed={true}
+									/>
 
-						</Cell>
-						<Cell col={4} style={{width: 'calc(50%)'}}>
-											<TextField
-											  hintText="Hint Text"
-											  floatingLabelText="Fixed Floating Label Text"
-											  floatingLabelFixed={true}
-											/>
-								<SelectFieldSimple />
-								
-								<DatePicker hintText="Portrait Dialog"  floatingLabelText="Ranged Date Picker" />							
-								
-	
-							</Cell>	
-						</Grid>	
+									<TextField
+									  hintText="Status"
+									  floatingLabelText="Status"
+									  floatingLabelFixed={true}
+									/>
+									<TextField
+									  hintText="Suspend Date"
+									  floatingLabelText="Suspend Date"
+									  floatingLabelFixed={true}
+									/>
+									<div >
+										<IconButton name = 'date_range' style = {{float: 'left', top: '30px', left:'220px', color:'rgba(0, 0, 0, 0.298039)' }}/>
+										<DatePicker 
+											floatingLabelText="Hired Date"
+											floatingLabelFixed={true}
+											hintText="Hired Date" 
+											container="inline" 
+											autoOk={true}
+											textFieldStyle = {{display: 'inherit', position: 'relative'}}
+										/>		
+									</div>
+									<SelectFieldSimple />
+									<SelectFieldDivision />
+									<TextField
+									  hintText="Email"
+									  floatingLabelText="Email"
+									  floatingLabelFixed={true}
+									/>
+									
+						
+						
+					</Cell>	
+					<Cell col={1} style={{minWidth: '100px'}}>							
+					<ImageUpload image='' style={{float:'right'}}/>
+					</Cell>
+				</Grid>	
+		  </div>
         </Dialog>
       </div>
     );
